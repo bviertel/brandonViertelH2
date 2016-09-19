25d6e6ec22ec76fac7495bc3ad32050788ccf8ca
Brandon <bviertel@gmail.com>
2016-09-19 17:08:04 -0400

    Initial Commit

---
+++ BabylonianMethod.java
@@ -0,0 +1,16 @@
+public class BabylonianMethod {
+  
+  private float firstGuess;
+  
+  private float lastGuess;
+  
+  // Trying to find the square root of 30
+  
+  public float BabylonianMethod(float num) {
+    
+    this.firstGuess = num;
+    
+    
+    
+    BabylonianMethod((guess/2)
+    
